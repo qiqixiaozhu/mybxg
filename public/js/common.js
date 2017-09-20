e3b7e0e5c7b06e849197ed7c13c06b4ea4cc4ffc
qiqixiaozhu <1317100347@qq.com>
2017-09-20 20:00:52 +0800

    模块化重构项目结构

--- public/js/common.js
+++ public/js/common.js
@@ -1,31 +1,36 @@
+define(['jquery','cookie'],function($){
+        // NProgress.start();
 
-NProgress.start();
+    // NProgress.done();
 
-NProgress.done();
-
-$('.navs ul').prev('a').on('click', function () {
-	$(this).next().slideToggle();
-});	
-	
-$("#logoutBtn").click(function(){
-            $.ajax({
-                type: 'post',
-                url: '/api/logout',
-                dataType: 'json',
-                success: function(data){
-                    console.log(data);
-                    if(data.code == 200){
-                        location.href='/main/login';
-                    }
+    //左侧菜单的切换
+    $('.navs ul').prev('a').on('click', function () {
+        $(this).next().slideToggle();
+    }); 
+    
+    //退出功能
+    $("#logoutBtn").click(function(){
+        $.ajax({
+            type: 'post',
+            url: '/api/logout',
+            dataType: 'json',
+            success: function(data){
+                //console.log(data);
+                if(data.code == 200){
+                    location.href='/main/login';
                 }
-            });
+            }
         });
+    });
 
-var flag=$.cookie('PHPSESSID');
-if(!flag){
-   location.href='/main/login';
-}
-var loginInfo=$.cookie('loginInfo');
-loginInfo=loginInfo&&JSON.parse(loginInfo);
-$('.aside profile img').attr('src',loginInfo.tc_avatar);
-$('.aside profile h4').html(loginInfo.tc_name);        
+    //判断是否登录
+    var flag=$.cookie('PHPSESSID');
+    if(!flag){
+        location.href='/main/login';
+    }
+    //登录成功 获取用户信息
+    var loginInfo=$.cookie('loginInfo');
+    loginInfo=loginInfo&&JSON.parse(loginInfo);
+    $('.aside profile img').attr('src',loginInfo.tc_avatar);
+    $('.aside profile h4').html(loginInfo.tc_name); 
+});
--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,9 @@
 require.config({
-	baseUrl:'',
+	baseUrl:'/public/assets',
 	paths:{
-		
+		jquery:'jquery/jquery.min',
+		cookie:'jquery-cookie/jquery.cookie',
+		common:'../js/common',
+		login:'../js/login'
 	}
 });
