b0618d0ff6e744c8f99aae0c270d6c50e0798d60
qiqixiaozhu <1317100347@qq.com>
2017-09-20 18:01:06 +0800

    退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,31 @@
 
-	NProgress.start();
+NProgress.start();
 
-	NProgress.done();
+NProgress.done();
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+$('.navs ul').prev('a').on('click', function () {
+	$(this).next().slideToggle();
+});	
+	
+$("#logoutBtn").click(function(){
+            $.ajax({
+                type: 'post',
+                url: '/api/logout',
+                dataType: 'json',
+                success: function(data){
+                    console.log(data);
+                    if(data.code == 200){
+                        location.href='/main/login';
+                    }
+                }
+            });
+        });
+
+var flag=$.cookie('PHPSESSID');
+if(!flag){
+   location.href='/main/login';
+}
+var loginInfo=$.cookie('loginInfo');
+loginInfo=loginInfo&&JSON.parse(loginInfo);
+$('.aside profile img').attr('src',loginInfo.tc_avatar);
+$('.aside profile h4').html(loginInfo.tc_name);        
