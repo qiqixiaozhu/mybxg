5bea9dc3feb587269be2744908b774e2f8dd3242
qiqixiaozhu <1317100347@qq.com>
2017-09-22 20:35:03 +0800

    表单验证插件重构表单验证功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','datepicker','language'],function($,template,util){
+define(['jquery','template','util','datepicker','language','validate','form'],function($,template,util){
 	var tcId=util.qs('tc_id');
 	if(tcId){
 		//编辑讲师
@@ -25,19 +25,41 @@ define(['jquery','template','util','datepicker','language'],function($,template,
 	}
 
 	//提交表单操作
+	// function submitForm(url){
+	// 	$('#teacherBtn').click(function(){
+	// 		$.ajax({
+	// 			type:'post',
+	// 			url:url,
+	// 			data:$("#teacherForm").serialize(),
+	// 			dataType:'json',
+	// 			success:function(data){
+	// 				if(data.code == 200){
+	// 					location.href='/teacher/list';
+	// 				}
+	// 			}
+	// 		});
+	// 	});
+	// }
+
 	function submitForm(url){
-		$('#teacherBtn').click(function(){
-			$.ajax({
-				type:'post',
-				url:url,
-				data:$("#teacherForm").serialize(),
-				dataType:'json',
-				success:function(data){
-					if(data.code == 200){
-						location.href='/teacher/list';
+		$('#teacherForm').validate({
+			sendForm:false,
+			valid:function(){
+				$(this).ajaxSubmit({
+					url:url,
+					dataType:'json',
+					success:function(data){
+						if(data.code == 200){
+							location.href='/teacher/list';
+						}
 					}
-				}
-			});
+				});
+			},
+			description:{
+				tcName:{required:'用户名不能为空'},
+				tcPass:{required:'密码不能为空',pattern:'密码为六位数'},
+				tcJoinDate:{required:'日期不能为空'}
+			}
 		});
 	}
 });
