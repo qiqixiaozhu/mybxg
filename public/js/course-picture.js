0cda3d9dc22f6ca0d74d83ae466eae827eba9912
qiqixiaozhu <1317100347@qq.com>
2017-09-26 16:43:31 +0800

    完成课程图片裁切功能

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','uploadify'],function($,template,util){
+define(['jquery','template','util','uploadify','jcrop','form'],function($,template,util){
 	//左侧菜单栏选中功能
 	util.setMenu('/course/add');
 
@@ -11,10 +11,12 @@ define(['jquery','template','util','uploadify'],function($,template,util){
 		data : {cs_id:csId},
 		dataType : 'json',
 		success : function(data){
-			if(data.code == 200){
+			
 				var html=template('pictureTpl',data.result);
 				$('#pictureInfo').html(html);
 
+				var img=$('.preview img');
+				var nowCrop=null;
 				//图片上传
 				$('#myfile').uploadify({
 					width : 70,
@@ -30,9 +32,70 @@ define(['jquery','template','util','uploadify'],function($,template,util){
 						// console.log(b);
 						var obj=JSON.parse(b);
 						$('.preview img').attr('src',obj.result.path);
+
+						cropImg();
+						$('#cropBtn').text('保存图片').attr('data-flag',true);
 					}
 				});
-			}
+
+				//图片裁切
+				$('#cropBtn').click(function(){
+					var flag=$(this).attr('data-flag');
+					if(flag){
+						$('#cropForm').ajaxSubmit({
+							type : 'post',
+							url : '/api/course/update/picture',
+							data : {cs_id : csId},
+							dataType : 'json',
+							success : function(data){
+								if(data.code == 200){
+									location.href='/course/lesson?cs_id='+data.result.cs_id;
+								}
+							}
+						});
+					}else{
+						$(this).text('保存图片').attr('data-flag',true);
+						cropImg();
+					}
+				});
+
+				//封装裁切图片方法
+				function cropImg(){
+					img.Jcrop({
+						aspectRatio : 2,
+					},function(){
+						// nowCrop || nowCrop.destroy();
+						// nowCrop=this;
+						//缩略图显示
+						this.initComponent('Thumbnailer',{width : 240,height :120,mythumb : '.thumb'});
+						// console.log(this);
+
+						//获取图片宽度
+						var width=this.ui.stage.width;
+						//获取图片高度
+						var height=this.ui.stage.height;
+
+						//选区数据
+						var x=0;
+						var y=(height-width/2)/2;
+						var w=width;
+						var h=width/2;
+						//创建选区
+						this.newSelection();
+						this.setSelect([x,y,w,h]);
+
+					});
+
+					//图片区域
+					img.parent().on('cropstart cropmove cropend',function(a,b,c){
+						var inputs=$('#cropForm').find('input');
+						inputs.eq(0).val(c.x);
+						inputs.eq(1).val(c.y);
+						inputs.eq(2).val(c.w);
+						inputs.eq(3).val(c.h);
+					})
+				}
+			
 		}
 	});
 });
